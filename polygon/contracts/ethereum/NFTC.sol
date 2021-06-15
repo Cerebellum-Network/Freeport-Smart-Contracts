07f79c7dda71645cd5bb84fc9b7814861f444e49
Aurélien Nicolas <info@nau.re>
2021-06-15 15:41:24 +0200

    Capture a fee per transfer

---
+++ polygon/contracts/ethereum/NFTC.sol
@@ -0,0 +1,59 @@
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+
+contract NFTC is ERC1155 {
+    uint256 public constant WCERE = 0;
+
+    address public serviceProvider;
+    uint256 public serviceFee;
+
+    constructor() ERC1155("https://cere.network/ntfc/{id}.json") {
+        serviceProvider = _msgSender();
+        serviceFee = 10;
+    }
+
+    /**
+     * @dev Hook that is called before any token transfer. This includes minting
+     * and burning, as well as batched variants.
+     *
+     * The same hook is called on both single and batched variants. For single
+     * transfers, the length of the `id` and `amount` arrays will be 1.
+     *
+     * Calling conditions (for each `id` and `amount` pair):
+     *
+     * - When `from` and `to` are both non-zero, `amount` of ``from``'s tokens
+     * of token type `id` will be  transferred to `to`.
+     * - When `from` is zero, `amount` tokens of token type `id` will be minted
+     * for `to`.
+     * - when `to` is zero, `amount` of ``from``'s tokens of token type `id`
+     * will be burned.
+     * - `from` and `to` are never both zero.
+     * - `ids` and `amounts` have the same, non-zero length.
+     *
+     * To learn more about hooks, head to xref:ROOT:extending-contracts.adoc#using-hooks[Using Hooks].
+     */
+    function _beforeTokenTransfer(
+        address operator,
+        address from,
+        address to,
+        uint256[] memory ids,
+        uint256[] memory amounts,
+        bytes memory data
+    )
+    internal
+    virtual
+    override
+    {
+        if(to == serviceProvider) return;
+
+        // Pay a fee per transfer to the service provider.
+        safeTransferFrom(
+            from,
+            serviceProvider,
+            WCERE,
+            serviceFee * ids.length,
+            ""
+        );
+    }
+}
--- polygon/truffle-config.polygon.js
+++ polygon/truffle-config.polygon.js
@@ -94,6 +94,7 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
+      version: "^0.8.0",
     }
   },
   db: {
