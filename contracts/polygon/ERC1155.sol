291a47b44cfd339198f6ececebde48189d3b8acb
Aurélien Nicolas <info@nau.re>
2021-06-23 21:52:51 +0200

    Modify ERC115 implementation to support forced transfers

--- contracts/polygon/ERC1155.sol
+++ contracts/polygon/ERC1155.sol
@@ -1,13 +1,14 @@
+// From @openzeppelin/contracts 4.1.0
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.0;
 
-import "./IERC1155.sol";
-import "./IERC1155Receiver.sol";
-import "./extensions/IERC1155MetadataURI.sol";
-import "../../utils/Address.sol";
-import "../../utils/Context.sol";
-import "../../utils/introspection/ERC165.sol";
+import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
+import "@openzeppelin/contracts/token/ERC1155/IERC1155Receiver.sol";
+import "@openzeppelin/contracts/token/ERC1155/extensions/IERC1155MetadataURI.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
+import "@openzeppelin/contracts/utils/Context.sol";
+import "@openzeppelin/contracts/utils/introspection/ERC165.sol";
 
 /**
  * @dev Implementation of the basic standard multi-token.
@@ -20,7 +21,7 @@ contract ERC1155 is Context, ERC165, IERC1155, IERC1155MetadataURI {
     using Address for address;
 
     // Mapping from token ID to account balances
-    mapping (uint256 => mapping(address => uint256)) private _balances;
+    mapping (uint256 => mapping(address => uint256)) _balances;
 
     // Mapping from account to operator approvals
     mapping (address => mapping(address => bool)) private _operatorApprovals;
