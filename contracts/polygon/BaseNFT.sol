1bdb2bcbd0e59bbf37648ee360bb1e4bedfc95cc
Aurélien Nicolas <info@nau.re>
2021-06-23 21:52:25 +0200

    Copy ERC1155 implementation

--- contracts/polygon/BaseNFT.sol
+++ contracts/polygon/BaseNFT.sol
@@ -1,14 +1,13 @@
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/utils/Context.sol";
-import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "./ERC1155.sol";
 
 /** An implementation of ERC1155 from OpenZeppelin.
 
 - Hold NFTs.
 - Hold a currency for fees.
 */
-contract BaseNFT is Context, ERC1155 {
+contract BaseNFT is ERC1155 {
     uint256 public constant CURRENCY = 0;
 
     constructor() ERC1155("https://cere.network/nft/{id}.json") {}
