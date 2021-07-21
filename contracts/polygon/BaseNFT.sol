6597add145d8339d36f9d4cc5e51d0ab399828d1
Aurélien Nicolas <info@nau.re>
2021-07-21 02:06:47 +0200

    full-operator: add role of full operator

--- contracts/polygon/BaseNFT.sol
+++ contracts/polygon/BaseNFT.sol
@@ -1,5 +1,6 @@
 pragma solidity ^0.8.0;
 
+import "./access/AccessControl.sol";
 import "./token/ERC1155/ERC1155.sol";
 
 /** An implementation of ERC1155 from OpenZeppelin.
@@ -7,7 +8,7 @@ import "./token/ERC1155/ERC1155.sol";
 - Hold NFTs.
 - Hold a currency for fees.
 */
-contract BaseNFT is ERC1155 {
+contract BaseNFT is AccessControl, ERC1155 {
     /** The token ID that represents the CERE currency for all payments in this contract. */
     uint256 public constant CURRENCY = 0;
 
@@ -15,6 +16,8 @@ contract BaseNFT is ERC1155 {
      * That is 10 billion tokens, with 10 decimals.
      */
     uint256 public constant CURRENCY_SUPPLY = 10e9 * 1e10;
+
+    bytes32 public constant FULL_OPERATOR = keccak256("FULL_OPERATOR");
 
     constructor() ERC1155("https://cere.network/nft/{id}.json") {}
 
@@ -31,5 +34,18 @@ contract BaseNFT is ERC1155 {
 
         address operator = _msgSender();
         emit TransferSingle(operator, from, to, id, amount);
+    }
+
+    /** The role FULL_OPERATOR is allowed to make any transfer.
+     * This is useful to connect a marketplace contract.
+     *
+     * @dev See {IERC1155-isApprovedForAll}.
+     */
+    function isApprovedForAll(address account, address operator)
+    public view virtual override returns (bool) {
+
+        if (hasRole(FULL_OPERATOR, operator)) return true;
+
+        return super.isApprovedForAll(account, operator);
     }
 }
