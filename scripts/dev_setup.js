7a19251520a6f66d89c13f2a2c5f1424c2b2be21
Aurélien Nicolas <info@nau.re>
2021-08-19 19:06:06 +0200

    fiat-gateway: dev setup

--- scripts/dev_setup.js
+++ scripts/dev_setup.js
@@ -5,12 +5,25 @@ const log = console.log;
 module.exports = async function (done) {
 
     let accounts = await web3.eth.getAccounts();
-    let gateway = await FiatGateway.deployed();
+    let admin = accounts[0];
     let davinci = await Davinci.deployed();
-    log("Contract", davinci.address, "from account", accounts[0]);
+    let gateway = await FiatGateway.deployed();
+    log("Operating on Davinci contract", davinci.address);
+    log("Operating on FiatGateway contract", gateway.address);
+    log("From admin account", admin);
+
+    const EXCHANGE_RATE_ORACLE = await gateway.EXCHANGE_RATE_ORACLE.call();
+    const PAYMENT_SERVICE = await gateway.PAYMENT_SERVICE.call();
+
+    log("Give the permission to change the exchange rate to Admin", admin);
+    await gateway.grantRole(EXCHANGE_RATE_ORACLE, admin);
+
+    log("Set an exchange rate of 0.1 token for $0.01");
+    await gateway.setExchangeRate(0.1e10);
 
     let devAccounts = [
         gateway.address,
+        "0xc0DAe4aE8d21250a830B2A79314c9D43cAeab145", // Fixed account for tests (mnemonic "spatial spin account funny glue cancel cushion twelve inmate author night dust").
         "0x6d2b28389d3153689c57909829dFCf6241d36388", // Evgeny
         "0x1Bf6FCa28253A1257e4B5B3440F7fbE0c59D1546", // Sergey
         "0x51c5590504251A5993Ba6A46246f87Fa0eaE5897", // Aurel
@@ -22,6 +35,9 @@ module.exports = async function (done) {
     for (let devAccount of devAccounts) {
         await davinci.deposit(devAccount, encodedAmount);
         log("Sent 100k of currency to", devAccount);
+
+        log("Give the permission to execute payments to", devAccount);
+        await gateway.grantRole(PAYMENT_SERVICE, devAccount);
     }
 
     done();
