b71b66160fc7c295b99a15a3228fc204bf5b68bd
Pavel Tabako <pavel.tabako@minsk.leveris.com>
2021-08-20 14:55:44 +0300

    - fix ecr repository name - add push to docker registry

--- run-ganache-deploy-copy-artifacts.sh
+++ run-ganache-deploy-copy-artifacts.sh
@@ -10,7 +10,7 @@ id=$(docker run -d \
   --network=bridge \
   -v "$PWD"/artifacts/db:/app/db \
   -v "$PWD"/artifacts/contracts:/app/davinci_nft/build/contracts \
-  338287888375.dkr.ecr.us-west-2.amazonaws.com/davinci-nft:builder \
+  338287888375.dkr.ecr.us-west-2.amazonaws.com/crb-davinci-nft-test:builder \
   --db /app/db \
   --mnemonic "$MNEMONIC" \
   --networkId "$NETWORK_ID")
