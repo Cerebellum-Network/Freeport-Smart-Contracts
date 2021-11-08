89696561f7f94b57bfdfc7fedba711ec880a1361
Aurélien Nicolas <info@nau.re>
2021-11-08 11:38:53 +0100

    attach-cid: contract to attach objects to NFTs

---
+++ test/attachment_test.js
@@ -0,0 +1,30 @@
+const Davinci = artifacts.require("./Davinci.sol");
+const NFTAttachment = artifacts.require("NFTAttachment");
+const log = console.log;
+const {expectEvent, expectRevert, constants, time} = require('@openzeppelin/test-helpers');
+const BN = require('bn.js');
+
+contract("NFTAttachment", accounts => {
+    const [sender] = accounts;
+
+    it("sells an NFT by auction", async () => {
+
+        const davinci = await Davinci.deployed();
+        const attachment = await NFTAttachment.deployed();
+
+        let gotDavinci = await attachment.nftContract.call();
+        assert.equal(davinci.address, gotDavinci);
+
+        let nftSupply = 10;
+        let nftId = await davinci.issue.call(nftSupply, "0x", {from: sender});
+
+        let cid = "0x1122334455667788990011223344556677889900112233445566778899001122"
+        let receipt = await attachment.attachToNFT(nftId, cid);
+
+        expectEvent(receipt, 'AttachToNFT', {
+            sender,
+            nftId,
+            cid,
+        });
+    });
+});
