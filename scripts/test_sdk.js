ae4451953cd4b9d5b36c762ba0fee9fede7fb617
Aurélien Nicolas <info@nau.re>
2022-01-18 13:40:13 +0100

    biconomy: test dev version of the SDK

--- scripts/test_sdk.js
+++ scripts/test_sdk.js
@@ -12,43 +12,45 @@ const waitOnBiconomy = (biconomy) => new Promise((resolve, reject) => {
 module.exports = async function (done) {
     try {
         let accounts = await web3.eth.getAccounts();
-        let truffleProvider = web3.currentProvider;
-        let ethersProvider; // = new ethers.providers.Web3Provider(truffleProvider);
+        //let truffleProvider = web3.currentProvider;
+        //let ethersProvider = new ethers.providers.Web3Provider(truffleProvider);
         let deployment = "dev";
 
-        // Pass connected wallet provider under walletProvider field
-        let biconomy = new Biconomy(truffleProvider,
-            {
-                walletProvider: truffleProvider,
-                apiKey: BICONOMY_API_KEY,
-                debug: true
-            });
+        const provider = await sdk.createProvider(
+            "https://rpc-mumbai.maticvigil.com",
+            true,
+            BICONOMY_API_KEY,
+        );
 
-        await waitOnBiconomy(biconomy);
-
-        ethersProvider = new ethers.providers.Web3Provider(biconomy);
-
+        log(provider);
 
         const freeportAddress = await sdk.getFreeportAddress(
-            ethersProvider,
+            provider,
             deployment,
         );
-        log("Freeport", freeportAddress);
+        log("Freeport address", freeportAddress);
 
         let freeport = sdk.createFreeport({
-            provider: ethersProvider,
+            provider,
             contractAddress: freeportAddress,
+            mnemonic: "typical cable potato lamp popular submit forget slow fiction promote pulp please",
         });
+        log("Freeport", freeport);
+
+        log("provider signer", provider.getSigner());
+        log("freeport provider signer", freeport.provider.getSigner());
 
         let tx = await freeport.issue(10, [0], {gasLimit: 1e6});
         log("TX", tx);
         let receipt = await tx.wait();
         log("RECEIPT", receipt);
-
         let event = receipt.events[0];
+        log("EVENT", event);
+
         console.assert(event.eventSignature === "TransferSingle(address,address,address,uint256,uint256)");
         console.assert(event.args.from === '0x0000000000000000000000000000000000000000');
         console.assert(event.args.to === accounts[0]);
+
 
         log("OK");
         done();
