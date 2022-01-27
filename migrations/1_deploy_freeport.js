3bb0454468f9693f9e805d1087ef2380b845b4c2
Aurélien Nicolas <info@nau.re>
2022-01-27 09:09:14 +0100

    upgradeable: adapt migration scripts

--- migrations/1_deploy_freeport.js
+++ migrations/1_deploy_freeport.js
@@ -1,8 +1,8 @@
-const {deployProxy} = require('@openzeppelin/truffle-upgrades');
 const Freeport = artifacts.require("Freeport");
+const {deployProxy} = require('@openzeppelin/truffle-upgrades');
 const log = console.log;
 
 module.exports = async function (deployer, network, accounts) {
-    const instance = await deployProxy(Freeport, [], {deployer, kind: "uups"});
-    log("Deployed Freeport proxy", instance.address);
+    const freeport = await deployProxy(Freeport, [], {deployer, kind: "uups"});
+    log("Deployed Freeport proxy", freeport.address);
 };
--- migrations/4_deploy_simple_auction.js
+++ migrations/4_deploy_simple_auction.js
@@ -1,16 +1,14 @@
-const {deployProxy} = require('@openzeppelin/truffle-upgrades');
 const Freeport = artifacts.require("Freeport");
 const SimpleAuction = artifacts.require("SimpleAuction");
+const {deployProxy} = require('@openzeppelin/truffle-upgrades');
 const log = console.log;
 
 module.exports = async function (deployer, network, accounts) {
     const freeport = await Freeport.deployed();
     log("Operating on Freeport contract", freeport.address);
 
-    const instance = await deployProxy(SimpleAuction, [freeport.address], {deployer, kind: "uups"});
-    log("Deployed SimpleAuction proxy", instance.address);
-    const auction = await SimpleAuction.deployed();
-    log("Operating on SimpleAuction contract", auction.address);
+    const auction = await deployProxy(SimpleAuction, [freeport.address], {deployer, kind: "uups"});
+    log("Deployed SimpleAuction proxy", auction.address);
 
     const TRANSFER_OPERATOR = await freeport.TRANSFER_OPERATOR.call();
 
