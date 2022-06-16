36b145ecc60d121029d3cff3f9a3980794d9d898
Anton Volk <anton.volk@cere.io>
2022-06-16 10:52:04 +0300

    FREEP-604: added SC name (for opensea compatibility)
    Fixing remarks.

--- migrations/11_deploy_collection_foundation.js
+++ migrations/11_deploy_collection_foundation.js
@@ -1,11 +1,12 @@
 const Freeport = artifacts.require("Freeport");
-const CollectionFoundation = artifacts.require("CollectionFoundation");
+const CollectionFactory = artifacts.require("CollectionFactory");
 const {deployProxy} = require('@openzeppelin/truffle-upgrades');
 const log = console.log;
 
 module.exports = async function (deployer, network, accounts) {
     const freeport = await Freeport.deployed();
-    const cf = await deployProxy(CollectionFoundation, [], {deployer, kind: "uups"});
+    log("Operating CollectionFactory with Freeport contract", freeport.address);
+    const cf = await deployProxy(CollectionFactory, [], {deployer, kind: "uups"});
     cf.contract.methods['setFreeport(address)'](freeport.address);
-    log("Deployed CollectionFoundation proxy", cf.address);
+    log("Deployed CollectionFactory proxy", cf.address);
 };
