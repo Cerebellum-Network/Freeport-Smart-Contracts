52676a51885fb3d34294d7538b31743001363270
Aurélien Nicolas <info@nau.re>
2022-08-29 19:28:17 +0200

    FREEP-605: test basic marketplace

--- test/collection_test.js
+++ test/collection_test.js
@@ -13,6 +13,7 @@ const BN = require('bn.js');
 contract("Collection", accounts => {
     const deployer = accounts[0];
     const minter = accounts[1];
+    const buyer = accounts[2];
 
     const CURRENCY = 0;
     const UNIT = 1e6;
@@ -68,26 +69,52 @@ contract("Collection", accounts => {
     });
 
     it("creates a collection", async () => {
-        let createTx = await factory.createCollection(minter, "my collection", "https://{id}.json", "https://my_contract");
+        // Deployer creates a collection for the minter.
+        let createTx = await factory.createCollection(minter, "my collection", "https://{id}.json", "https://my_contract", {from: deployer});
 
         let collectionAddr = getEvent(createTx, "CollectionCreated").addr;
 
+        // The minter mints some NFTs.
         let mintTx = await factory.mintOnBehalf(collectionAddr, minter, 10, "0x", {from: minter});
 
         let mintEvent = getEvent(mintTx, "MintOnBehalf");
+        let nftId = mintEvent._id;
         assert.equal(mintEvent._operator, minter);
         assert.equal(mintEvent._collection, collectionAddr);
         assert.equal(mintEvent._holder, minter);
         assert.equal(mintEvent._amount, 10);
 
-        let parsedId = await factory.parseNftId(mintEvent._id);
+        let parsedId = await factory.parseNftId(nftId);
         assert.equal(parsedId.collection, collectionAddr);
         assert.equal(parsedId.innerId.toNumber(), 1);
         assert.equal(parsedId.supply.toNumber(), 0);
 
         let collection = await Collection.at(collectionAddr);
-        let balance = await collection.balanceOf(minter, mintEvent._id);
+        let balance = await collection.balanceOf(minter, nftId);
         assert.equal(balance.toNumber(), 10);
+
+        // The minter puts the NFTs for sale.
+        let price = 100;
+        let makeOfferTx = await marketplace.makeOffer(nftId, price, {from: minter});
+        let makeOfferEv = getEvent(makeOfferTx, "MakeOffer");
+        assert.equal(makeOfferEv.seller, minter);
+        assert.equal(makeOfferEv.nftId.toString(), nftId.toString());
+        assert.equal(makeOfferEv.price.toNumber(), price);
+
+        // The buyer gets some money.
+        await erc20.mint(buyer, 1000);
+
+        // The buyer approves the marketplace.
+        await erc20.approve(marketplace.address, aLot, {from: buyer});
+
+        // The buyer buys one NFT.
+        let takeOfferTx = await marketplace.takeOffer(buyer, minter, nftId, 0, 2, {from: buyer});
+        let takeOfferEv = getEvent(takeOfferTx, "TakeOffer");
+        assert.equal(takeOfferEv.buyer, buyer);
+        assert.equal(takeOfferEv.seller, minter);
+        assert.equal(takeOfferEv.nftId.toString(), nftId.toString());
+        assert.equal(takeOfferEv.price.toNumber(), price);
+        assert.equal(takeOfferEv.amount.toNumber(), 2);
     });
 
 });
