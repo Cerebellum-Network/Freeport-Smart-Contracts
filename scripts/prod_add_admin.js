bb127b08d751a5abce30ecae6d21f57a6a368883
Aurélien Nicolas <info@nau.re>
2022-09-01 08:10:17 +0200

    FREEP-605: fix prod_add_admin script

--- scripts/prod_add_admin.js
+++ scripts/prod_add_admin.js
@@ -8,17 +8,17 @@ const log = console.log;
 
 module.exports = async function (done) {
 
-    const gnosis = "0xB537f7D82b1c3625b29Da794017937F5D0A9b3c6";
+    const gnosis = "0x87A0892F98914c567379475a806A1419143406F6";
 
     const accounts = await web3.eth.getAccounts();
     const admin = accounts[0];
     log("From admin account", admin);
-    let freeport = await Freeport.at("0x702BA959B5542B2Bf88a1C5924F73Ed97482c64B");
-    let gateway = await FiatGateway.at("0x106Bf3D61952faE9279B08bdcB2e548316E0C1Ae");
-    let auction = await SimpleAuction.at("0xCEAc6c102bEcE4ed2E5ede9df096F7175BB8CbaD");
-    let attachment = await NFTAttachment.at("0x39B27a0bc81C1366E2b05E02642Ef343a4f9223a");
-    let collectionFactory = await CollectionFactory.at("0xc4018973d7ae51b4669dDe7948904d034A79Bb86");
-    let marketplace = await CollectionFactory.at("0xB0D3aF64CD682aF52d5Dd77D2402EE253be38107");
+    let freeport = await Freeport.deployed();
+    let gateway = await FiatGateway.deployed();
+    let auction = await SimpleAuction.deployed();
+    let attachment = await NFTAttachment.deployed();
+    let collectionFactory = await CollectionFactory.deployed();
+    let marketplace = await Marketplace.deployed();
     const DEFAULT_ADMIN_ROLE = await freeport.DEFAULT_ADMIN_ROLE.call();
 
     const contracts = [
